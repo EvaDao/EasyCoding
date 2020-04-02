f06973641067ba7d6682de248faed89a33184516
Eva Zhang <ZHANEVA@chrobinson.com>
2020-04-02 18:03:23 +0800

    at the begining

---
+++ src/EasyCoding.SimpleTypes.Extension/StringExts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCoding.SimpleTypes.Extension
+{
+    public static class StringExts
+    {
+        public static bool IsNullOrEmpty(this string value)
+            => string.IsNullOrEmpty(value);
+    }
+}
---
+++ tests/EasyCoding.SimpleTypes.Extensions.Tests/StringExtsShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyCoding.SimpleTypes.Extensions.Tests
+{
+    //[TestClass]
+    //public class StringExtsShould
+    //{
+    //    [TestMethod]
+    //    public void IsNullOrEmpty_Null_ShouldSameAsOrigion()
+    //    {
+    //        string value = null;
+    //        value.IsNullOrEmpty().Should()
+    //    }
+    //}
+}
